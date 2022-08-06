12a9bdb9641758df302e69ce76d0c5f9420823f8
samantha <samantha1999@live.com.sg>
2022-08-06 21:05:02 +0800

    change navigation colour and added icons

--- finalProject/src/pages/MainRoot.js
+++ finalProject/src/pages/MainRoot.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Constants from 'expo-constants';
 import Home from './Home';
 import Favourites from './Favourites';
@@ -11,10 +12,45 @@ const Tab = createBottomTabNavigator();
 class MainRoot extends Component{
     render() {
         return(
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Favourites" component={Favourites} />
-                <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Navigator 
+                screenOptions = {{
+                    tabBarActiveTintColor: "#84DCC6",
+                    tabBarInactiveTintColor: "#95A3B3",
+                }}
+                initialRouteName = "Home"
+            >
+                <Tab.Screen
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarIcon:({ color, size }) => (
+                            <MaterialCommunityIcons name="home" color={color} size={size} />
+                        ),
+                    }}
+                    name="Home"
+                    component={Home}
+                    />
+
+                <Tab.Screen
+                    options={{
+                        tabBarLabel: "Favourites",
+                        tabBarIcon:({ color, size }) => (
+                            <MaterialCommunityIcons name="cards-heart" color={color} size={size} />
+                        ),
+                    }} 
+                    name="Favourites" 
+                    component={Favourites}
+                />
+
+                <Tab.Screen 
+                    options={{
+                        tabBarLabel: "Settings",
+                        tabBarIcon:({ color, size }) => (
+                            <MaterialCommunityIcons name="cog" color={color} size={size} />
+                        ),
+                    }} 
+                name="Settings" 
+                component={Settings} 
+                />
             </Tab.Navigator>
         )
     }
