f250d7c94d857ab9924b95af92d1d7ebb8fe8a65
samantha <samantha1999@live.com.sg>
2022-08-07 21:24:27 +0800

    able to display image

--- finalProject/src/components/MovieComp.js
+++ finalProject/src/components/MovieComp.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
 
 function MovieComp(props){
     return(
-        <View>
+        <View style={styles.container}>
+            <Image style={styles.poster} source={{uri:"http://image.tmdb.org/t/p/w342/" + props.item.poster_path}} />
             <Text>{props.item.title}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:"column",
+        flexWrap:"wrap",
+    },
+    poster:{
+        width:342,
+        height:513,
+    }
+});
 
 export default MovieComp;
