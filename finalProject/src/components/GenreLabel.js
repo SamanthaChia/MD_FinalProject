c44c7ef732eb0903db3688b7d457bd6144abf17b
samantha <samantha1999@live.com.sg>
2022-08-14 21:07:57 +0800

    movie details

---
+++ finalProject/src/components/GenreLabel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+function GenreLabel(props){
+    return(
+        <View style={styles.itemGroup}>
+            {
+                props.data.map((item, index) => {
+                    return(
+                        <View style={styles.label} key={index}>
+                            <Text style={styles.genreText}>{item}</Text>
+                        </View>
+                    )
+                })
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    itemGroup:{
+        flexDirection: "row",
+    },
+    label:{
+        backgroundColor:"#333",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 16,
+        marginRight: 10,
+    },
+
+    genreText:{
+        color: "white",
+    }
+})
+
+export default GenreLabel;
--- finalProject/src/pages/MovieDetails.js
+++ finalProject/src/pages/MovieDetails.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import Constants from 'expo-constants';
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import GenreLabel from '../components/GenreLabel';
 
 function MovieDetails({navigation, route}){
+    const movieDetails = route.params.details;
+    
     return(
         <View style={styles.container}>
-            {/* Retrieve from navigation, using route parms, and retrieving the 
-            params passed into details. */}
-            <Text>{route.params.details.title}</Text>
+            <ScrollView>
+                <TouchableWithoutFeedback onPress = { () => navigation.pop()}>
+                    <MaterialCommunityIcons 
+                        style={{
+                            position:"absolute", 
+                            top:Constants.statusBarHeight + 10, 
+                            left:10, 
+                            zIndex: 1, 
+                            paddingRight: 10,
+                            paddingBottom: 20,
+                            }}
+                        name="arrow-left"
+                        size={24}
+                        color={"#fff"}
+                    />
+                </TouchableWithoutFeedback>
+                <Image style={styles.poster} resizeMode={"cover"} source={{uri:"http://image.tmdb.org/t/p/w500/" + movieDetails.poster_path}} />
+                {/* Retrieve from navigation, using route parms, and retrieving the 
+                params passed into details. */}
+                <View style={styles.movie_information}>
+                    <View style={styles.outer_box}>
+                        <View style={styles.title_genre}>
+                            <Text>{movieDetails.title}</Text>
+                            <Text>{movieDetails.release_date}</Text>
+                        </View>
+                        <View style={styles.averageScore}>
+                            <Text>{movieDetails.vote_average}</Text>
+                        </View>
+                    </View>
+                    <GenreLabel data={movieDetails.genre} />
+                    <Text style={styles.header}>Movie Description</Text>
+                    <Text>{movieDetails.overview}</Text>
+                    <Text style={styles.header}>Cast</Text>
+                    <Text style={styles.header}>Movie Trailers & Teasers</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -16,9 +52,38 @@ function MovieDetails({navigation, route}){
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        padding: 5,
-        marginTop: Constants.statusBarHeight,
     },
+    poster:{
+        height:230,
+    },
+    movie_information:{
+        flex: 1,
+        backgroundColor: "grey",
+        padding: 20,
+    },
+    outer_box:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent:"space-between",
+        alignItems:"center"
+    },
+    title_genre:{
+        flexWrap:"wrap",
+        flexDirection:"column"
+    },
+    averageScore:{
+        width:48,
+        height:48,
+        backgroundColor:"white",
+        borderRadius: 24,
+        justifyContent: "center",
+        alignItems:"center",
+    },
+    header:{
+        fontSize: 23,
+        fontWeight: "bold",
+        marginTop: 10,
+    }
 });
 
 export default MovieDetails;
