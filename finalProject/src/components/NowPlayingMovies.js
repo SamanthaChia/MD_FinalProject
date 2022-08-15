c5076317e38372c5426924d8f3152017230be01d
samantha <samantha1999@live.com.sg>
2022-08-15 14:06:31 +0800

    rating added

--- finalProject/src/components/NowPlayingMovies.js
+++ finalProject/src/components/NowPlayingMovies.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
-
+import { View, Text, StyleSheet, Image, TouchableWithoutFeedback, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 function NowPlayingMovies(props){
+    const deviceWidth = Dimensions.get("window").width;
+    var widthVal = (deviceWidth - 60) / 2;
 
     const navigation = useNavigation();
 
     return(
-        //  pass details for props through navigation, as details.
+        // pass details for props through navigation, as details.
         <TouchableWithoutFeedback onPress= {() => navigation.navigate('MovieDetails', { details: props.item })}>
             <View style={styles.container}>
-                <Image style={styles.poster} source={{uri:"http://image.tmdb.org/t/p/w342/" + props.item.poster_path}} />
-                <Text>{props.item.title}</Text>
+                <Image style={[styles.poster, {width:widthVal}]} source={{uri:"http://image.tmdb.org/t/p/w342/" + props.item.poster_path}} />
+                <View style={styles.descriptionBox}>
+                    <Text style={{width:widthVal}}>{props.item.title}</Text>
+                    <View style={styles.ratingAvg}>
+                        <MaterialCommunityIcons name="star" size={20} color="#EDD622" />
+                        <Text style={{fontWeight: "bold", alignSelf:"center"}}>{props.item.vote_average}</Text>
+                        <Text style={{fontSize:10, alignSelf:"flex-end"}}> /10</Text> 
+                    </View>
+                </View>
             </View>
         </TouchableWithoutFeedback>
     );
@@ -20,15 +30,20 @@ function NowPlayingMovies(props){
 
 const styles = StyleSheet.create({
     container:{
-        flexDirection:"column",
+        flexDirection:"row",
         flexWrap:"wrap",
-        marginRight:10,
     },
     poster:{
         borderRadius:10,
-        width:166,
         height:255,
         marginBottom:10,
+    },
+    descriptionBox:{
+        marginLeft: 10,
+    },
+    ratingAvg:{
+        flexDirection: "row",
+        flexWrap: "wrap",
     }
 });
 
