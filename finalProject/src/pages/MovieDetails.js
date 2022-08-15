8c6ba1d1bdbda67da4bfbf90132c970531c773b0
samantha <samantha1999@live.com.sg>
2022-08-15 17:56:41 +0800

    movie details dark and lightmode

--- finalProject/src/pages/MovieDetails.js
+++ finalProject/src/pages/MovieDetails.js
@@ -6,6 +6,7 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import GenreLabel from '../components/GenreLabel';
 import TrailerTeaser from '../models/TrailerTeaser';
 import TrailerTeaserDisplay from '../components/TrailerTeaserDisplay';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class MovieDetails extends Component{
     // initialise
@@ -50,91 +51,98 @@ class MovieDetails extends Component{
    render(){
 
     return(
-        <View style={styles.container}>
-            <Modal 
-                style={styles.modal}
-                animationType="slide"
-                transparent={true}
-                statusBarTranslucent={true}
-                visible={this.state.modalVisible}
-                onRequestClose={()=>{
-                    this.setState({ modalVisible: false});
-                }}
-            >
-                <View style={styles.modalBox}>
-                    <View style={styles.xBtn}>
-                        <TouchableWithoutFeedback onPress = { () => this.setState({modalVisible: false})}>
-                        <MaterialCommunityIcons 
-                            name="close"
-                            size={20}
-                            color={"white"}
-                        />
-                    </TouchableWithoutFeedback>
-                    </View>
-                    <View style={{width: "100%"}}>
-                        <YoutubePlayer
-                            height={300}
-                            play={true}
-                            videoId={this.state.activeMovieTrailerKey}
-                        />
-                     </View>
-                </View>
-            </Modal>
-            <ScrollView>
-                <TouchableWithoutFeedback onPress = { () => this.props.navigation.pop()}>
-                    <MaterialCommunityIcons 
-                        style={{
-                            position:"absolute", 
-                            top:Constants.statusBarHeight + 10, 
-                            left:10, 
-                            zIndex: 1, 
-                            paddingRight: 10,
-                            paddingBottom: 20,
+        <ThemeContext.Consumer>
+            {(context) => {
+                const { boolDarkMode, light, dark } = context;
+                return(
+                    <View style={[styles.container,{backgroundColor:boolDarkMode ? dark.bg : light.bg}]}>
+                        <Modal 
+                            style={styles.modal}
+                            animationType="slide"
+                            transparent={true}
+                            statusBarTranslucent={true}
+                            visible={this.state.modalVisible}
+                            onRequestClose={()=>{
+                                this.setState({ modalVisible: false});
                             }}
-                        name="arrow-left"
-                        size={24}
-                        color={"#fff"}
-                    />
-                </TouchableWithoutFeedback>
-                <Image style={styles.poster} resizeMode={"cover"} source={{uri:"http://image.tmdb.org/t/p/w500/" + this.movieDetails.poster_path}} />
-                {/* Retrieve from navigation, using route parms, and retrieving the 
-                params passed into details. */}
-                <View style={styles.movie_information}>
-                    <View style={styles.outer_box}>
-                        <View style={styles.title_genre}>
-                            <Text style={styles.title}>{this.movieDetails.title}</Text>
-                            <Text>{this.movieDetails.release_date}</Text>
-                        </View>
-                        <View style={styles.averageScore}>
-                            <Text>{this.movieDetails.vote_average}</Text>
-                        </View>
+                        >
+                            <View style={styles.modalBox}>
+                                <View style={styles.xBtn}>
+                                    <TouchableWithoutFeedback onPress = { () => this.setState({modalVisible: false})}>
+                                    <MaterialCommunityIcons 
+                                        name="close"
+                                        size={20}
+                                        color={"white"}
+                                    />
+                                </TouchableWithoutFeedback>
+                                </View>
+                                <View style={{width: "100%"}}>
+                                    <YoutubePlayer
+                                        height={300}
+                                        play={true}
+                                        videoId={this.state.activeMovieTrailerKey}
+                                    />
+                                </View>
+                            </View>
+                        </Modal>
+                        <ScrollView>
+                            <TouchableWithoutFeedback onPress = { () => this.props.navigation.pop()}>
+                                <MaterialCommunityIcons 
+                                    style={{
+                                        position:"absolute", 
+                                        top:Constants.statusBarHeight + 10, 
+                                        left:10, 
+                                        zIndex: 1, 
+                                        paddingRight: 10,
+                                        paddingBottom: 20,
+                                        }}
+                                    name="arrow-left"
+                                    size={24}
+                                    color={"#fff"}
+                                />
+                            </TouchableWithoutFeedback>
+                            <Image style={styles.poster} resizeMode={"cover"} source={{uri:"http://image.tmdb.org/t/p/w500/" + this.movieDetails.poster_path}} />
+                            {/* Retrieve from navigation, using route parms, and retrieving the 
+                            params passed into details. */}
+                            <View style={styles.movie_information}>
+                                <View style={styles.outer_box}>
+                                    <View style={styles.title_genre}>
+                                        <Text style={[styles.title,{color:boolDarkMode ? light.bg : dark.bg}]}>{this.movieDetails.title}</Text>
+                                        <Text style={{color:boolDarkMode ? light.bg : dark.bg}}>{this.movieDetails.release_date}</Text>
+                                    </View>
+                                    <View style={styles.averageScore}>
+                                        <Text>{this.movieDetails.vote_average}</Text>
+                                    </View>
+                                </View>
+                                <GenreLabel data={this.movieDetails.genre} />
+                                <Text style={[styles.header,{color:boolDarkMode ? light.bg : dark.bg}]}>Movie Description</Text>
+                                <Text style={{color:boolDarkMode ? light.bg : dark.bg}}>{this.movieDetails.overview}</Text>
+                                <Text style={[styles.header, {color:boolDarkMode ? light.bg : dark.bg}]}>Cast</Text>
+                                <Text style={[styles.header, {color:boolDarkMode ? light.bg : dark.bg}]}>Movie Trailers & Teasers</Text>
+                                <View style={styles.trailerTeaserBox}>
+                                    {
+                                        this.state.trailerTeasers.map((item) => {
+                                            return (
+                                                    <TrailerTeaserDisplay 
+                                                        key={item.key}
+                                                        onPressFunction={()=> this.setState({
+                                                            modalVisible:true,
+                                                            activeMovieTrailerKey: item.key
+                                                        })}
+                                                        poster={this.movieDetails.poster_path}
+                                                        trailerdata={item}
+                                                        modalVisible={this.state.modalVisible}
+                                                    />
+                                            )
+                                        })
+                                    }
+                                </View>
+                            </View>
+                        </ScrollView>
                     </View>
-                    <GenreLabel data={this.movieDetails.genre} />
-                    <Text style={styles.header}>Movie Description</Text>
-                    <Text>{this.movieDetails.overview}</Text>
-                    <Text style={styles.header}>Cast</Text>
-                    <Text style={styles.header}>Movie Trailers & Teasers</Text>
-                    <View style={styles.trailerTeaserBox}>
-                        {
-                            this.state.trailerTeasers.map((item) => {
-                                return (
-                                        <TrailerTeaserDisplay 
-                                            key={item.key}
-                                            onPressFunction={()=> this.setState({
-                                                modalVisible:true,
-                                                activeMovieTrailerKey: item.key
-                                            })}
-                                            poster={this.movieDetails.poster_path}
-                                            trailerdata={item}
-                                            modalVisible={this.state.modalVisible}
-                                        />
-                                )
-                            })
-                        }
-                    </View>
-                </View>
-            </ScrollView>
-        </View>
+                )
+            }}
+        </ThemeContext.Consumer>
     );
    }
 }
