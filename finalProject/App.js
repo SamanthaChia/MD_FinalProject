5d15e5c235a59336cf3a2769193794f9d044978d
samantha <samantha1999@live.com.sg>
2022-08-16 12:25:49 +0800

    clean up code

--- finalProject/App.js
+++ finalProject/App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';  
 import MainRoot from './src/pages/MainRoot';
@@ -37,12 +34,3 @@ export default function App() {
     </ThemeContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- finalProject/src/pages/Home.js
+++ finalProject/src/pages/Home.js
@@ -203,7 +203,6 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         padding: 5,
-        marginTop: Constants.statusBarHeight,
     },
     popularHome:{
         flexDirection: "row",
